4a6edc6fc4f7ca878c9ec06f7599e29293054625
Liliana Leiva <liliana-carmen@hotmail.com>
2022-08-02 21:02:22 -0300

    login users controller

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,5 +1,6 @@
 const { Operation, User, Category } = require("../db.js");
 const { Sequelize } = require('sequelize');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 
@@ -7,9 +8,32 @@ const login = async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        if (!username || !password) {
+            return res.status(422).json('Username and password are both required');
+        }
 
+        const user = await User.findOne({
+            where: { username: username }
+        })
 
-        return res.status(200).json(operations);
+        if (user) {
+            const correctPassword = await bcrypt.compare(password, user.password);
+
+            if (correctPassword) {
+                const userToken = {
+                    id: user.id,
+                }
+                const token = jwt.sign(userToken, process.env.SECRETWORD, {
+                    expiresIn: 60 * 60 * 24 * 7,
+                })
+                return res.status(200).send({
+                    username,
+                    token
+                });
+            }
+        } else {
+            return res.status(200).send('User or password is invalid');
+        }
     } catch (error) {
         console.log(error)
         return res.status(404).json({ error: 'There was an error...' });
@@ -20,25 +44,27 @@ const register = async (req, res) => {
     const { username, password } = req.body;
 
     try {
-        const userFound = await User.findOne({    
-            where: { username: username}
+        if (!username || !password) {
+            return res.status(422).json('Username and password are both required');
+        }
+
+        const userFound = await User.findOne({
+            where: { username: username }
         })
 
         if (userFound) {
-            return res.status(201).send('User already exists');
+            return res.status(200).send('User already exists');
         } else {
             const saltRounds = 10;
             const hash = bcrypt.hashSync(password, saltRounds)
-            const newUser = await User.create({
+            await User.create({
                 username,
                 password: hash
             })
-                
             return res.status(201).send('User created');
         }
     } catch (error) {
-        console.log(error)
-        // return res.status(404).json({ error: 'There was an error...' });
+        return res.status(404).json({ error: 'There was an error...' });
     }
 }
 
