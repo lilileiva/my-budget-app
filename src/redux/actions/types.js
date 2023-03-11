713f9524fb0c6d073de7c424168500e9d254388c
lilileiva <liliana-carmen@hotmail.com>
2023-03-11 19:18:05 -0300

    update client and api

--- src/redux/actions/types.js
+++
@@ -1,5 +0,0 @@
-// export const BASE_URL = 'http://localhost:3001/my_budget_app';
-export const BASE_URL = 'https://my-budget-app-api-dp.onrender.com/my_budget_app';
-
-export const GET_TRANSACTIONS = "GET_TRANSACTIONS";
-export const GET_CATEGORIES = "GET_CATEGORIES";
