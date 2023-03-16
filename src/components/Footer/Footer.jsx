99d28b5a2faa9695909d5f354ca73ccf88faf61e
lilileiva <liliana-carmen@hotmail.com>
2023-03-16 09:01:08 -0300

    client

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+      Created by Liliana Leiva. 2022
+    </div>
+  )
+}
+
+
+export default Footer;
